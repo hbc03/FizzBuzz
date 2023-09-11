d4063cbfd9ad92e2d0ac0197e416b32b62ea965c
hbc03 <hboracelebi2003@gmail.com>
2023-09-11 16:28:01 -0400

    read me change

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,11 +8,11 @@ class FizzBuzz {
 
         while ( i < 100) {
 
-            i = diFizzBuzz(i);
+            i = doFizzBuzz();
         }
     }
 
-    private static int diFizzBuzz(int i) {
+    private static int doFizzBuzz() {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
